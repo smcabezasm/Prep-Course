eea8fdd817b161fe0efc7aa8893249fc097d95ec
smcabezasm12 <smcabezasm@gmail.com>
2022-12-17 03:13:49 -0500

    modified

--- 03-JS-II/homework/homework.js
+++ 03-JS-II/homework/homework.js
@@ -5,12 +5,22 @@ function obtenerMayor(x, y) {
   // Devuelve el número más grande
   // Si son iguales, devuelve cualquiera de los dos
   // Tu código:
+  if (x>y){
+    return x;
+  }
+  return y;
 }
 
 function mayoriaDeEdad(edad) {
   //Determinar si la persona según su edad puede ingresar a un evento.
   //Si tiene 18 años ó más, devolver --> "Allowed"
   //Si es menor, devolver --> "Not allowed"
+  if (edad>=18) {
+    return "Allowed";
+  }
+  else {
+    return "Not allowed";
+  }
 }
   
 function conection(status) {
@@ -19,6 +29,15 @@ function conection(status) {
   //Cuando el estado es igual a 2, el usuario está "Away"
   //De lo contrario, presumimos que el usuario está "Offline"
   //Devolver el estado de conexión de usuario en cada uno de los casos.
+  if (status=1){
+    return "Online";
+  }
+  else if (status=2){
+    return "Away";
+  }
+  else {
+    return "Offline";
+  }
 }
 
 function saludo(idioma) {
@@ -28,6 +47,18 @@ function saludo(idioma) {
   // Si "idioma" es "ingles", devuelve "Hello!"
   // Si "idioma" no es ninguno de los anteiores o es `undefined` devuelve "Hola!"
   // Tu código:
+  if (saludo="aleman"){
+    return "Guten Tag";
+  }
+  else if (saludo="mandarin"){
+    return "Ni Hao";
+  }
+  else if (Saludo="ingles"){
+    return "Hello";
+  }
+  else{
+    return "Hola"
+  }
 }
 
 function colors(color) {
